3d39660b7a3f26e0e5248bf86019c55ff30b0a74
argshub <argshub@outlook.com>
2020-11-24 01:15:26 -0800

    Add files via upload

--- post_all_word.php
+++ post_all_word.php
@@ -1,4 +1,14 @@
 <?php
 $data = file_get_contents("php://input");
+$data = json_decode($data, true);
+if(strlen($data['image'])) {
+   $imageData = $data['image'];
+   $imageSrcData = "";
+   while (!$imageSrcData) {
+       $imageSrcData = file_get_contents($imageData);
+   }
+   $data['image'] = base64_encode($imageSrcData);
+}
+$data = json_encode($data);
 $file = fopen("etob.txt", "a");
 fwrite($file, $data . "\n");
